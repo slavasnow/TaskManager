85d6635fdc7920bf1ec59653a4a6cd3df44364dc
slavasnow <slavash21@yandex.ru>
2023-01-14 15:08:27 +0300

    Добавлена возможность делать таски

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,4 +1,5 @@
-extends Button
+extends Node2D
+
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -6,8 +7,8 @@ extends Button
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
+func _ready():
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +17,12 @@ extends Button
 
 
 func _on_AddTaskBtn_pressed():
-
-	#var text = $TextTask.text
+	var _text = $TextTask.text
 	var task = preload("res://Scens/Task.tscn").instance()
 
-	#print (text)
-	get_node("/root/main/TaskContainer").add_child(task)
+	task.get_node("Label").text = _text
+	get_node("TaskContainer").add_child(task)
 
-	#pass # Replace with function body.
+	$TextTask.text = ""
+
+	pass # Replace with function body.
